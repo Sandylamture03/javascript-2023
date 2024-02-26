ff3373d86a00a509cc631366aaa618170d89ef19
Sandy_Lamture <sandylamture03@gmail.com>
2024-02-26 17:42:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all JS code as newer version.
+
+//alert("hellow");  // we are using node.js , not browser
+
+let name = "sandeep"
+let age = 30
+let isLoggedIn = false
+
+//premetive datatypes
+// number
+// bigint
+//boolen => true/false
+// string => ""
+// null => standlone value
+// undefined => 
+// symbol => used for uniqueness
+
+//Non-premitive datatypes
+// object =>
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
