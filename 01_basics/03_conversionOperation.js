85ce5de67dce8ec212bc59905dfb7f9f0c8c5211
Sandy_Lamture <sandylamture03@gmail.com>
2024-02-26 18:25:30 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,44 @@ console.log(booleanIsLoggedIn);
  let someNumber =33;
  let stringNumber = String(someNumber)
  console.log(typeof stringNumber);
+
+
+ // ***************************operations***************
+
+ let value = 3
+ let negValue = -value
+ //console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+ let str1 = "hello"
+
+ let str2 = "sandy"
+
+ let str3 = str1 + str2;
+ 
+ console.log(str3); // hello sandy
+
+ console.log("1"+2); // 12
+ console.log(1+"2");// 12
+ console.log("1"+2+2);//122
+ console.log(1+2+"2"); //32
+
+ console.log(+true);// 1
+ console.log(+"");//0
+
+ let gameCounter = 100;
+ gameCounter++;
+ console.log(gameCounter);//101
+
+ let gameCounter1 = 100;
+ ++gameCounter1;
+ console.log(gameCounter1);//101
+
+
+
