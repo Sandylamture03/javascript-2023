88df90cc3cf41b13817cc309ae794dd0e0dfff70
Sandy_Lamture <sandylamture03@gmail.com>
2024-02-26 18:01:43 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" = 33
+// "33abc"= NaN
+// true = 1
+// undefine = NaN 
+
+let isLoggedIn = true;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+ // 1 = true; 0 = fales
+ //"" = false
+ // "hitest" = true
+
+
+ let someNumber =33;
+ let stringNumber = String(someNumber)
+ console.log(typeof stringNumber);
