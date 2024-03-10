34f44b38d3baae8bbee557fcb90bf2468427cb2b
Sandy_Lamture <sandylamture03@gmail.com>
2024-03-10 12:52:37 +0000

    some chnages made

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,30 +7,30 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);  // s
 
 const gameName = new String("Sandeep-l-com"); // another away to define string
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]); // S
+console.log(gameName.__proto__); // {}
 
-console.log(gameName.length);
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('d'));
-console.log(gameName.toUpperCase());
+console.log(gameName.length); //13
+console.log(gameName.charAt(2)); // n
+console.log(gameName.indexOf('d')); // 3
+console.log(gameName.toUpperCase()); // SANDEEP-L-COM
 
-const newString = gameName.substring(0,4);
-console.log(newString);
+const newString = gameName.substring(0,4); 
+console.log(newString); // Sand
 
 
 const anotherString = gameName.slice(-8,4);
 console.log(anotherString);
 
 const newStringOne = "    sandeep    ";
-console.log(newStringOne.trim());
+console.log(newStringOne.trim()); //Sandeep
 
 
 const url = "https://lco.com/sandeep%20lamture";
-console.log(url.replace('%20','-'));
+console.log(url.replace('%20','-')); //  https://lco.com/sandeep-lamture
 
 
-console.log(url.includes('sandeep'));
-console.log(url.includes('hitesh'));
+console.log(url.includes('sandeep')); //true
+console.log(url.includes('hitesh')); // false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); // [ 'Sandeep', 'l', 'com' ]
